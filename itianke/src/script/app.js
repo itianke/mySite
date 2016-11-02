ee370ed78f6b08104a2bf3bb0094aacece929a6c
tianke <tianke@oak-inc.com>
2016-11-02 11:16:17 +0800

    some Edit

---
+++ itianke/src/script/app.js
@@ -0,0 +1,6 @@
+var contact_id = document.getElementById('contact_id');
+var contact_info = document.getElementById('contact_info');
+
+contact_id.onclick = function(){
+	contact_info.style.display = "block";
+}
